30a899a298a4ebb4b0c65f8750508d93eec8a982
sereneblue <sereneblue@users.noreply.github.com>
2022-05-13 19:27:38 -0400

    feat: add skeleton routes

--- omni.py
+++ omni.py
@@ -1,7 +1,25 @@
-from bottle import route, run
+from bottle import Bottle, get, post, run
 
-@route('/')
+app = Bottle()
+
+@app.get('/')
 def home():
-    return "Hello World!"
+    return ""
 
-run(host='localhost', port=8080, debug=True)
+@app.get('/api/devices')
+def devices():
+    return ""
+
+@app.get('/api/applications/<device_id>')
+def get_applications(device_id=""):
+    return ""
+
+@app.get('/api/log/<log_type>')
+def get_logs(log_type=""):
+    return ""
+
+@app.post('/api/action/<device_id>')
+def app_action(device_id=""):
+    return ""
+
+run(app, host='localhost', port=8080, debug=False)
