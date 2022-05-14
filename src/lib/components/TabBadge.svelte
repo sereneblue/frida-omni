659fa8ed9ae1ceaa2737813b5d93299630cf1c8c
sereneblue <sereneblue@users.noreply.github.com>
2022-05-14 19:55:03 -0400

    feat: add tab badge component

---
+++ src/lib/components/TabBadge.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let count = 0;
+
+	const readableNumber = (num: number): string => {
+	    var s = ['', 'K', 'M', 'B'];
+	    var e = Math.floor(Math.log(num) / Math.log(1000));
+
+	    return (((num / Math.pow(1000, e)).toPrecision(3)) / 1) + s[e];
+	}
+</script>
+
+{#if count > 0}
+	<span class="rounded-full bg-teal-600 text-xs text-white font-bold opacity-100 px-1.5 py-0.5">
+		{readableNumber(count)}
+	</span>
+{/if}
