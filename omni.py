b5330fb5d625e1f2830a6c97ac3633e8100c2488
sereneblue <sereneblue@users.noreply.github.com>
2022-05-14 22:28:02 -0400

    feat: add sqlite3 database to persist log data and log api

--- omni.py
+++ omni.py
@@ -1,30 +1,26 @@
-from bottle import Bottle, get, post, run, response
+from bottle import Bottle, get, post, request, response, run 
 import bottle
 import base64
 import frida
 import operator
 import struct
+import sqlite3
 
-class EnableCors(object):
-    name = 'enable_cors'
-    api = 2
-
-    def apply(self, fn, context):
-        def _enable_cors(*args, **kwargs):
-            response.headers['Access-Control-Allow-Origin'] = '*'
-            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
-            response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
-
-            if bottle.request.method != 'OPTIONS':
-                return fn(*args, **kwargs)
-
-        return _enable_cors
+import omni_log
 
 app = Bottle()
+db = sqlite3.connect(":memory:", check_same_thread=False)
+db.row_factory = sqlite3.Row
+cur = db.cursor()
 
 @app.get('/')
 def home():
     return ""
+
+@app.route('/', method = 'OPTIONS')
+@app.route('/<path:path>', method = 'OPTIONS')
+def options_handler(path = None):
+    return
 
 @app.get('/api/devices')
 def devices():
@@ -67,13 +63,82 @@ def get_applications(device_id=""):
 
     return dict(data=application_list, success=True, message="")
 
-@app.get('/api/log/<log_type>')
-def get_logs(log_type=""):
-    return ""
+@app.post('/api/logs')
+def get_logs():
+    totals = {}
+    logs = {}
+
+    for log in request.json['logs']:
+        log_data = []
+
+        if log == 'hash':
+            cur.execute("select * from log_hash order by id desc limit 100;")
+            
+            for row in cur:
+                log_data.append({
+                    'id': row['id'],
+                    'algo': row['algo'],
+                    'timestamp': row['timestamp'],
+                    'input': row['input'],
+                    'output': row['output']
+                })
+        elif log == 'pkg_info':
+            log_data = {}
+
+            cur.execute("select * from log_pkg_info")
+            for row in cur:
+                if row['type'] == 'metadata':
+                    log_data[row['name']] = row['value']
+                else:
+                    if log_data.get(row['type']) == None:
+                        log_data[row['type']] = []
+
+                    log_data[row['type']].append({
+                        'name': row['name'],
+                        'value': row['value']
+                        })
+        else:
+            pass
+
+        logs[log] = log_data
+
+        if type(log_data) == list and len(log_data) > 0:
+            totals[log] = log_data[0]['id']
+        else:
+            totals[log] = 0
+
+    return dict(data={"logs": logs, "totals": totals}, success=True, message="")
 
 @app.post('/api/action/<device_id>')
 def app_action(device_id=""):
     return ""
+
+@app.hook('after_request')
+def enable_cors():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Origin, Accept, Content-Type, X-Requested-With'
+
+def create_db():
+    cur.executescript("""
+        create table log_pkg_info (
+            type TEXT NOT NULL,
+            name TEXT NOT NULL,
+            value TEXT NOT NULL
+        );
+
+        create index log_pkg_info_idx on log_pkg_info (type);
+
+        create table log_hash (
+            id INTEGER PRIMARY KEY,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            algo TEXT NOT NULL,
+            input TEXT NOT NULL,
+            output TEXT NOT NULL
+        );
+
+        create index log_hash_idx on log_hash (algo);
+    """)
 
 def get_icon(app_params):
     icons = app_params['icons'][0] if app_params.get('icons') else None
@@ -84,5 +149,14 @@ def get_icon(app_params):
 
     return None
 
-app.install(EnableCors())
+def on_message(message, data):
+    if message['type'] == 'send':
+        if message['payload']['log'] == 'hash':
+            omni_log.log_hash(cur, message['payload'])
+        if message['payload']['log'] == 'pkg_info':
+            omni_log.log_pkg_info(cur, message['payload'])
+    else:
+        print(message)
+
+create_db()
 run(app, host='localhost', port=8080, debug=True)
