5c7e1fcca319b256796fc8d430f388792513e814
sereneblue <sereneblue@users.noreply.github.com>
2022-05-16 19:15:43 -0400

    feat: add views for hash and sqlite

---
+++ src/lib/components/HashView.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import OmniTable from "./OmniTable.svelte";
+
+	export let data = [];
+
+	const headers = ["Timestamp", "Hash", "Input", "Output"];
+	const render = [
+		{
+			fn: (data) => data.timestamp,
+			clipboard: false,
+		},
+		{
+			fn: (data) => data.algo,
+			clipboard: false,
+		},
+		{
+			fn: (data) => data.input,
+			clipboard: true,
+		},
+		{
+			fn: (data) => data.output,
+			clipboard: true,
+		},
+	]
+</script>
+
+<div>
+	<OmniTable {headers} {data} {render} />
+</div>
---
+++ src/lib/components/SQLiteView.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { HashData } from "../types.ts";
+	import OmniTable from "./OmniTable.svelte";
+
+	export let data = [];
+
+	const headers = ["DB", "Method", "Value"];
+	const render = [
+		{
+			fn: (data) => data.db,
+			clipboard: false,
+		},
+		{
+			fn: (data) => data.method,
+			clipboard: false,
+		},
+		{
+			fn: (data) => data.value,
+			clipboard: true,
+		}
+	]
+</script>
+
+<div>
+	<OmniTable {headers} {data} {render} />
+</div>
