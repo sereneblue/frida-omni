ed97f46d2150bc4c22bc403fc5b0c27835f1f8d7
sereneblue <sereneblue@users.noreply.github.com>
2022-05-16 19:15:02 -0400

    feat: add method to get logs

--- src/lib/omni.ts
+++ src/lib/omni.ts
@@ -3,6 +3,15 @@ import type { ResponseData } from "./types";
 class Omni {
 	static base_url: string = "http://localhost:8080";
 	base_url: string = "http://localhost:8080";
+	logs: object;
+
+	constructor() {
+		this.logs = {
+			pkg_info: true,
+			hash: true,
+			sqlite: true
+		}
+	}
 
 	static async getApps(deviceId: string): Promise<ResponseData> {
 		let res = await fetch(this.base_url + `/api/applications/${deviceId}`);
@@ -13,6 +22,23 @@ class Omni {
 
 	static async getDevices(): Promise<ResponseData> {
 		let res = await fetch(this.base_url + '/api/devices');
+		let data = await res.json();
+
+		return data;
+	}
+
+	async getLogData(): Promise<ResponseData> {
+		let enabledLogs = { logs : Object.keys(this.logs).filter(k => this.logs[k]) }
+
+		let res = await fetch(this.base_url + '/api/logs', {
+		    method: 'POST',
+		    headers: {
+		      'Accept': 'application/json',
+		      'Content-Type': 'application/json'
+		    },
+		    body: JSON.stringify(enabledLogs)
+		  });
+
 		let data = await res.json();
 
 		return data;
